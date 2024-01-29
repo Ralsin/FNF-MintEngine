46e38f5c7e25da0e06f34341fea2f93724a09fa7
Ralsi <61703859+Ralsin@users.noreply.github.com>
2024-01-29 23:50:48 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,12 +9,13 @@ import openfl.events.Event;
 using StringTools;
 
 class Main extends Sprite {
-	public static var fpsVar:FPS;
-	public static var ramVar:Memory;
+	public static var instance(default, null):Main = new Main();
+	public static var fpsVar:FPS = new FPS();
+	public static var ramVar:Memory = new Memory();
 
 	public static function main():Void {
-		// Application.current.window.borderless = true;
-		Lib.current.addChild(new Main());
+		hl.UI.closeConsole();
+		Lib.current.addChild(instance);
 	}
 
 	public function new() {
@@ -31,8 +32,8 @@ class Main extends Sprite {
 			removeEventListener(Event.ADDED_TO_STAGE, init);
 
 		addChild(new FlxGame(1280, 720, MainState, 144, 144, false, false));
-		fpsVar = new FPS();
-		ramVar = new Memory();
+		if (flixel.FlxG.save.data.volume != null)
+			flixel.FlxG.sound.volume = flixel.FlxG.save.data.volume;
 		addChild(fpsVar);
 		addChild(ramVar);
 		// Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(openfl.events.UncaughtErrorEvent.UNCAUGHT_ERROR, (event:openfl.events.UncaughtErrorEvent) -> {
